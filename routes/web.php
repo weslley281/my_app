d83a76edca499700cc90a01b632783c5c6cdfe14
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2024-07-18 13:20:42 -0400

    a

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,44 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::view('/home', 'home', ['title' => 'Olá Mundo']);
+
+
 Route::get('/users', function () {
+    return [
+        [
+            "name" => "Fulano",
+            "age" => 31,
+            "weigth" => "81.54",
+            "is_admin" => true,
+            "address" => [
+                "address_line1" => "Rua Sei lá, casa 32",
+                "address_line2" => "Quadra 12",
+                "city" => "Cuaibá",
+            ]
+        ],
+        [
+            "name" => "Cicrano",
+            "age" => 27,
+            "weigth" => "72.64",
+            "is_admin" => false,
+            "address" => [
+                "address_line1" => "Rua Sei lá, casa 32",
+                "address_line2" => "Quadra 12",
+                "city" => "Cuaibá",
+            ]
+        ]
+    ];
+})->name("usuarios");
+
+Route::post('/users', function () {
     return "Olá mundo";
+});
+
+Route::delete('/users', function () {
+    return "Olá mundo";
+});
+
+Route::get('rota-a', function () {
+    return redirect("users");
 });
