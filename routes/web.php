37a2b45c6705bcccefe12e59d266a716d142a064
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2024-07-18 17:18:35 -0400

    k

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,35 @@ Route::get('/', function () {
 
 Route::view('/home', 'home', ['title' => 'Olá Mundo']);
 
+Route::get('/users/{id}', function ($id) {
+    $users = [
+        [
+            "name" => "Fulano",
+            "age" => 31,
+            "weigth" => "81.54",
+            "is_admin" => true,
+            "address" => [
+                "address_line1" => "Rua Sei lá, casa 32",
+                "address_line2" => "Quadra 12",
+                "city" => "Cuaibá",
+            ]
+        ],
+        [
+            "name" => "Cicrano",
+            "age" => 27,
+            "weigth" => "72.64",
+            "is_admin" => false,
+            "address" => [
+                "address_line1" => "Rua Sei lá, casa 32",
+                "address_line2" => "Quadra 12",
+                "city" => "Cuaibá",
+            ]
+        ]
+    ];
+
+    return $users[$id];
+})->whereNumber('id');
+
 
 Route::get('/users', function () {
     return [
